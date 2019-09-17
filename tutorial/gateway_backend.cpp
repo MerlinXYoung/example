b5d63f499b98b3dbd10dce4175117c3ea3bea516
MerlinXYoung <merlin_young@163.com>
2019-09-17 11:21:46 +0800

    build ok

--- tutorial/gateway_backend.cpp
+++ tutorial/gateway_backend.cpp
@@ -20,13 +20,22 @@ int main(int argc, char** argv)
             if (items[0].revents & ZMQ_POLLIN) {
                 // printf("\n%s ", identity);
                 // s_dump(client_socket_);
+                zmq::message_t message_client;
+                server.recv(&message_client);
+                int size = message_client.size();
+                int more = 0;
+                size_t more_size = sizeof (more);
+                server.getsockopt(ZMQ_RCVMORE, &more, &more_size);
+                if(!more || size != sizeof(uint32_t))
+                {
+                    printf("fuck message not more or  not client id\n");
+                    continue;
+                }
+                uint32_t client_id = *reinterpret_cast<uint32_t*>(message_client.data());//atol(str_client_id.c_str());
                 zmq::message_t message;
                 server.recv(&message);
-                int size = message.size();
-                std::string str_client_id(static_cast<char*>(message.data()), sizeof(uint32_t));
-                uint32_t client_id = atol(str_client_id.c_str());
-
-                std::string data(static_cast<char*>(message.data()+sizeof(uint32_t)), size-sizeof(uint32_t));
+                size = message.size();
+                std::string data(reinterpret_cast<char*>(message.data()), size);
                 printf("msg[%u]:%s\n", client_id, data.c_str());
                 std::string rsp = data+" rsp!";
                 server.send(&client_id, sizeof(client_id), ZMQ_SNDMORE);
