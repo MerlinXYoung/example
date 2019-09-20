7185410f7fb340f4f0ee77e5c46ffa1f6e9d895b
MerlinXYoung <merlin_young@163.com>
2019-09-20 15:00:48 +0800

    b

--- gateway/protocol/cs_gateway_utility.h
+++ gateway/protocol/cs_gateway_utility.h
@@ -12,12 +12,14 @@ public:
     ReqParser():pkg_(nullptr),pkg_len_(0){}
     ~ReqParser()=default;
     int64_t check(char* base, uint32_t len);
-    EMsgID parse(Head& head, google::protobuf::message*&);
+    EMsgID parse(Head& head, google::protobuf::Message*&);
     inline void reset()
     {
         pkg_ = nullptr;
         pkg_len_ = 0;
     }
+    inline char* pkg(){return pkg_;}
+    inline uint32_t pkg_len()const{return pkg_len_;}
 
 private:
     char* pkg_;
